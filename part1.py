6d7c2fc2bd763033b1e6e77448f527435901c74b
Thomas <thomas.saltzman@gmail.com>
2018-06-28 23:27:23 -0500

    worked on part1 and part 4. more work needs to be done.

--- part1.py
+++ part1.py
@@ -1,48 +1,103 @@
 #%%
 import numpy as np 
 import matplotlib.pyplot as plt 
-import matplotlib
-matplotlib.use('TkAgg')
-from mpl_toolkits.mplot3d import Axes3D
 from sklearn.model_selection import train_test_split
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-
+from sklearn.linear_model import LogisticRegression
 from sklearn.datasets import load_iris
 
-lda = LinearDiscriminantAnalysis(n_components=3)
-data = load_iris()
+lr = LogisticRegression()
+lda = LinearDiscriminantAnalysis(n_components=4)
 
+data = load_iris()
+#separate the data and the target
 iris_x = data.data
 iris_y = data.target
-target_names = data.target_names
 
-x_train, x_test, y_train, y_test = train_test_split(iris_x,iris_y, test_size=0.9)
+#splits the data into test and train sets. set to  50/50 to get more incorrect results
+x_train, x_test, y_train, y_test = train_test_split(iris_x,iris_y, test_size=0.5)
 
-
+#train the data
 lda_train = lda.fit(x_train,y_train)
-lda_prediction = lda.predict(x_test)
-lda_ranges = lda.transform(x_test)
+lr.fit(x_train, y_train)
 
-end = x_test.shape[0]
+#array of the predict results
+lda_prediction = lda.predict(x_test)
+lr_prediction = lr.predict(x_test)
+
+#finds the number of data points to test
+end = lda_prediction.shape[0]
+
+#colors for marking thd data on the plot
 colors = ['navy', 'turquoise', 'orange']
-fig = plt.figure(1, figsize=(8, 6))
-ax = Axes3D(fig, elev=-150, azim=110)
+
+
+
+
+#plots the results of the LRA
+plt.figure(1)
 for i in range(end):
     if lda_prediction[i] != y_test[i]:
         color = 'red'
+        #print(x_test[i][0], x_test[i][1])
     else:
         color = colors[y_test[i]]
+    plt.scatter(x_test[i][0], x_test[i][1], c=color)
 
-    ax.scatter(x_test[i][0], x_test[i][1],x_test[i][2], c=color)
+#sets the labels
+plt.xlabel("Sepal Length (cm)")
+plt.ylabel("Sepal Width (cm)")
+plt.title('LDA of IRIS dataset "Sepal"')  
+    
+
+plt.figure(2)
+for i in range(end):    
+    if lda_prediction[i] != y_test[i]:
+        color = 'red'
+        #print(x_test[i][2], x_test[i][3])
+    else:
+         color = colors[y_test[i]]
+    
+    plt.scatter(x_test[i][2], x_test[i][3], c=color)
+
+#sets the labels
+plt.xlabel("Petal Length (cm)")
+plt.ylabel("Petal Width (cm)")
+plt.title('LDA of IRIS dataset "Petals"')    
 
 
+#plots the LR results
+plt.figure(3)
 
-#print(str(ranges[0][0]) + " " + str(ranges[0][1]))
-#print(str(ranges[1][0]) + " " + str(ranges[1][1]))
-#print(ranges)
+for i in range(end):
+    if lr_prediction[i] != y_test[i]:
+        color = 'red'
+        #print(x_test[i][0], x_test[i][1])
+    else:
+        color = colors[y_test[i]]
+    plt.scatter(x_test[i][0], x_test[i][1], c=color)
 
+#sets the labels
+plt.xlabel("Sepal Length (cm)")
+plt.ylabel("Sepal Width (cm)")
+plt.title('LR of IRIS dataset "Sepal"')  
+    
 
+plt.figure(4)
 
-#plt.legend(loc='best', shadow=False, scatterpoints=1)
-plt.title('LDA of IRIS dataset')
+for i in range(end):    
+    if lr_prediction[i] != y_test[i]:
+        color = 'red'
+        #print(x_test[i][2], x_test[i][3])
+    else:
+         color = colors[y_test[i]]
+    
+    plt.scatter(x_test[i][2], x_test[i][3], c=color)
+
+#sets the label
+plt.xlabel("Petal Length (cm)")
+plt.ylabel("Petal Width (cm)")
+plt.title('LR of IRIS dataset "Petals"')    
+
 plt.show()
+
---
+++ part4.py
@@ -0,0 +1,22 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_digits
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+data = load_digits()
+digit_x = data.data
+digit_y = data.target
+x_train, x_test, y_train, y_test = train_test_split(digit_x, digit_y, test_size=0.33)
+
+accuracy = []
+for x in range(1,50):
+    knn = KNeighborsClassifier(x)
+    knn.fit(x_train,y_train)
+    pred = knn.predict(x_test)
+    acc = accuracy_score(pred,y_test)
+    accuracy.append(acc)
+
+plt.plot([i for i in range(1,50)],accuracy, color='red')
+plt.show()
