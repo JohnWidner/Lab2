91c8da48d54ca4180e78e5c1c7857a3c2ee5254a
Thomas Saltzman <thomas.saltzman@gmail.com>
2018-06-29 11:26:22 -0500

    improved the part1 code added pictures

--- part1.py
+++ part1.py
@@ -6,6 +6,26 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.linear_model import LogisticRegression
 from sklearn.datasets import load_iris
 
+
+#colors for marking thd data on the plot. red will be used to mark the wrong predictions
+colors = ['navy', 'turquoise', 'orange']
+# used to plot all results
+def plot_results(prediction,data,test, xlabel:str ,ylabel: str,title: str, index: int):
+    #figure auto increases
+    plt.figure()
+    for i, p, t in zip(range(len(test)),prediction, test):
+        if p != t:
+            color = 'red'
+        else:
+            color = colors[t]
+        plt.scatter(x_test[i][index], x_test[i][index + 1], c=color)
+    
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)  
+
+
+
 lr = LogisticRegression()
 lda = LinearDiscriminantAnalysis(n_components=4)
 
@@ -25,79 +45,37 @@ lr.fit(x_train, y_train)
 lda_prediction = lda.predict(x_test)
 lr_prediction = lr.predict(x_test)
 
-#finds the number of data points to test
-end = lda_prediction.shape[0]
-
-#colors for marking thd data on the plot
-colors = ['navy', 'turquoise', 'orange']
-
-
-
-
-#plots the results of the LRA
-plt.figure(1)
-for i in range(end):
-    if lda_prediction[i] != y_test[i]:
-        color = 'red'
-        #print(x_test[i][0], x_test[i][1])
-    else:
-        color = colors[y_test[i]]
-    plt.scatter(x_test[i][0], x_test[i][1], c=color)
 
 #sets the labels
-plt.xlabel("Sepal Length (cm)")
-plt.ylabel("Sepal Width (cm)")
-plt.title('LDA of IRIS dataset "Sepal"')  
-    
-
-plt.figure(2)
-for i in range(end):    
-    if lda_prediction[i] != y_test[i]:
-        color = 'red'
-        #print(x_test[i][2], x_test[i][3])
-    else:
-         color = colors[y_test[i]]
-    
-    plt.scatter(x_test[i][2], x_test[i][3], c=color)
+xlabel = "Sepal Length (cm)"
+ylabel = "Sepal Width (cm)"
+title = 'LDA of IRIS dataset "Sepal"'
+#plots LDA for sepal
+plot_results(lda_prediction,x_test,y_test,xlabel,ylabel,title,0)
 
 #sets the labels
 plt.xlabel("Petal Length (cm)")
 plt.ylabel("Petal Width (cm)")
-plt.title('LDA of IRIS dataset "Petals"')    
+plt.title('LDA of IRIS dataset "Petals"') 
+#plots LDA for petals   
+plot_results(lda_prediction,x_test,y_test,xlabel,ylabel,title,2)
 
-
-#plots the LR results
-plt.figure(3)
-
-for i in range(end):
-    if lr_prediction[i] != y_test[i]:
-        color = 'red'
-        #print(x_test[i][0], x_test[i][1])
-    else:
-        color = colors[y_test[i]]
-    plt.scatter(x_test[i][0], x_test[i][1], c=color)
 
 #sets the labels
-plt.xlabel("Sepal Length (cm)")
-plt.ylabel("Sepal Width (cm)")
-plt.title('LR of IRIS dataset "Sepal"')  
+xlabel = "Sepal Length (cm)"
+ylabel = "Sepal Width (cm)"
+title = 'LR of IRIS dataset "Sepal"' 
+#plots LR for sepal
+plot_results(lr_prediction,x_test,y_test,xlabel,ylabel,title,0)
     
 
-plt.figure(4)
-
-for i in range(end):    
-    if lr_prediction[i] != y_test[i]:
-        color = 'red'
-        #print(x_test[i][2], x_test[i][3])
-    else:
-         color = colors[y_test[i]]
-    
-    plt.scatter(x_test[i][2], x_test[i][3], c=color)
 
 #sets the label
-plt.xlabel("Petal Length (cm)")
-plt.ylabel("Petal Width (cm)")
-plt.title('LR of IRIS dataset "Petals"')    
+xlabel = "Petal Length (cm)"
+ylabel = "Petal Width (cm)"
+title ='LR of IRIS dataset "Petals"'
+#plots LR for petal
+plot_results(lr_prediction,x_test,y_test,xlabel,ylabel,title,2)
 
 plt.show()
 
